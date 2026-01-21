77a5ec6a7a0b121e1291fa6d65827a566c527149
Frank Bedoya <frankbedoyamejia@gmail.com>
2026-01-21 18:33:04 -0500

    Corrijo import NavBar en tree.jsx

--- src/pages/tree.jsx
+++ src/pages/tree.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import VerticalTree from "../components/VerticalTree";
 import data from "../data/articlesTree.json";
 
 export default function TreePage() {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <h1 className="p-4 text-xl font-bold">Árbol Jerárquico de Artículos</h1>
       <VerticalTree data={data} width={1200} height={800} />
     </div>
